4c192da7eab87700bbd603a0c21f6fd04a0d9fc6
DaisyLBell <daisybel@usc.edu>
2022-07-16 21:23:32 -0700

    gradient-bg, and hover events

---
+++ home.js
@@ -0,0 +1,9 @@
+"use strict";
+$(document).ready(function(){
+    
+    $('#home-ibdStrong-og').hover(function(){
+        
+        $('.alt-ibdStrong-off').toggleClass("alt-ibdStrong-on");
+    });
+    
+});
