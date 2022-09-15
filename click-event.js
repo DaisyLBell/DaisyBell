38e4912195ad270d34e1b691e475778bc7bb9abb
DaisyLBell <daisybel@usc.edu>
2022-09-15 16:26:44 -0700

    changed the portfolio

---
+++ click-event.js
@@ -0,0 +1,26 @@
+var textButton = document.querySelector('.click-text');
+
+var classOff = document.querySelector('.click-off');
+
+var textOff = document.querySelector('.text-on');
+
+var classTurnOff;
+
+
+textButton.addEventListener('click', function () {
+    classOff.classList.add('click-off');
+//    textOff.classList.add('text-off');
+});
+
+textButton.addEventListener('click', function () {
+    classOff.classList.remove('click-off');
+//     textOff.classList.remove('text-off');                                           
+                                            });
+
+textButton.addEventListener('click',function(){
+    textOff.classList.add('text-off');
+});
+
+//classOff.addEventListener('click', function(){
+//    textOff.classList.remove('text-off');
+//});
