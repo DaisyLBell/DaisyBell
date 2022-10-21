1eebee0db8e6750c3ef60213a52bbfe5174ead27
DaisyLBell <daisybel@usc.edu>
2022-10-20 18:34:38 -0700

    responsive web design for tablets and phones

--- project-hover.js
+++ project-hover.js
@@ -6,28 +6,47 @@
 //    })
 //})
 
-$(document).ready(function(){
-    $('.project1').hover(function(){
+$(document).ready(function () {
+    $('.project1').hover(function () {
         $('.project-1-Text').toggleClass('project-1-Text-on');
     });
-    
-    $('.project2').hover(function(){
+
+    $('.project2').hover(function () {
         $('.project-2-Text').toggleClass('project-2-Text-on');
     });
-    
-    $('.project3').hover(function(){
+
+    $('.project3').hover(function () {
         $('.project-3-Text').toggleClass('project-3-Text-on');
     });
-    
-    $('.project4').hover(function(){
+
+    $('.project4').hover(function () {
         $('.project-4-Text').toggleClass('project-4-Text-on');
     });
-    
-    $('.project5').hover(function(){
+
+    $('.project5').hover(function () {
         $('.project-5-Text').toggleClass('project-5-Text-on');
     });
-    
-    $('.project6').hover(function(){
+
+    $('.project6').hover(function () {
         $('.project-6-Text').toggleClass('project-6-Text-on');
     });
-})
+
+    var menuOpen = document.getElementById('menuOpen');
+
+    var menuClose = document.getElementById('menuClose');
+
+    var menu = document.getElementById('menu');
+    
+    let x = 8;
+
+    menuOpen.addEventListener('click', function () {
+        menu.style.display = "block";
+       
+        console.log(x);
+    });
+
+    menuClose.addEventListener('click', function () {
+        menu.style.display = "none";
+    });
+
+});
