af6367a070aa175e3d646fe3cb1312c5a435a859
DaisyLBell <daisybel@usc.edu>
2023-04-18 20:28:56 -0700

    some updates but not all

--- scroll-to-top.js
+++ scroll-to-top.js
@@ -5,11 +5,16 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
+//if (doocument.body.scrollToTop > 2000 || document.doocumentElement.scrollTop > 2000) {
+//    mybutton.style.display = "none";
+//} else {
+//    mybutton.style.display = "none";
+//}
 }
 
 // When the user clicks on the button, scroll to the top of the document
--- showZines.js
+++ showZines.js
@@ -1,31 +1,31 @@
-var showSpectrum = document.getElementById('show-spectrum');
-var showRenaissance = document.getElementById('show-renaissance');
-
-
-showSpectrum.addEventListener('click', function(){
-    document.querySelector(".spectrum-body").style.display = "block";
-});
-
-showRenaissance.addEventListener('click', function(){
-    document.querySelector(".renaissance-body").style.display = "block";
-        document.querySelector("#renaissance").style.marginTop = "3vw";
-
-});
-
-var closeSpectrum = document.getElementById('close-spectrum');
-var closeRenaissance = document.getElementById('close-renaissance');
-
-closeSpectrum.addEventListener('click', function(){
-    
-    document.querySelector(".spectrum-body").style.display = "none";
-    
-});
-
-closeRenaissance.addEventListener('click', function(){
-    
-    document.querySelector(".renaissance-body").style.display = "none";
-    
-});
+//var showSpectrum = document.getElementById('show-spectrum');
+//var showRenaissance = document.getElementById('show-renaissance');
+//
+//
+//showSpectrum.addEventListener('click', function(){
+//    document.querySelector(".spectrum-body").style.display = "block";
+//});
+//
+//showRenaissance.addEventListener('click', function(){
+//    document.querySelector(".renaissance-body").style.display = "block";
+//        document.querySelector("#renaissance").style.marginTop = "3vw";
+//
+//});
+//
+//var closeSpectrum = document.getElementById('close-spectrum');
+//var closeRenaissance = document.getElementById('close-renaissance');
+//
+//closeSpectrum.addEventListener('click', function(){
+//    
+//    document.querySelector(".spectrum-body").style.display = "none";
+//    
+//});
+//
+//closeRenaissance.addEventListener('click', function(){
+//    
+//    document.querySelector(".renaissance-body").style.display = "none";
+//    
+//});
 
  var menuOpen = document.getElementById('menuOpen');
 
