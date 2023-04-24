b0b69c63486adb2205fdd7b4b655be2d68087cae
DaisyLBell <daisybel@usc.edu>
2023-04-24 16:47:28 -0700

    bar at bottom and scroll button ends

--- scroll-to-top.js
+++ scroll-to-top.js
@@ -4,18 +4,51 @@ let mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+
+
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-//if (doocument.body.scrollToTop > 2000 || document.doocumentElement.scrollTop > 2000) {
+//  if (document.documentElement.scrollTop <= 2000 ) {
+//      
+//    mybutton.style.display = "block";
+//      
+//  } else if (document.documentElement.scrollTop <= 20 ) {
+//      
 //    mybutton.style.display = "none";
-//} else {
+//      
+//  } else {
+//      
 //    mybutton.style.display = "none";
-//}
+//  }
+    
+    if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20){
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+
 }
+
+//function scrollFunction() {
+//    switch (document.body.scrollTop) {
+//            
+//        case 0:
+//            if (document.body.scrollTop < 20 ) {
+//            mybutton.style.display = "none";}
+//            break;
+//            
+//        case 1:
+//            if ( document.body.scrollTop > 20) {
+//            mybutton.style.display = "block";}
+//            break;
+//            
+//        case 2:
+//            if ( document.body.scrollTop > 1200 ) {
+//            mybutton.style.display = "none";
+//            break;}
+//    }
+//}
+
+
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
